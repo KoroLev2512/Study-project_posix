b51317812879ef79ac613ae92056d9800007c569
Korolev Yurii <yurii@niuitmo.ru>
2023-05-03 15:40:57 +0300

    version 3

--- producer_consumer.cpp
+++ producer_consumer.cpp
@@ -1,35 +1,114 @@
 #include <pthread.h>
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <cstdlib>
+#include <ctime>
+
+
+pthread_mutex_t MUTEX = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t CV_CONSUMER = PTHREAD_COND_INITIALIZER;
+pthread_cond_t CV_PRODUCER = PTHREAD_COND_INITIALIZER;
+
+std::queue<int> buffer;
+
+static const int BUFFER_SIZE = 5;
 
 int get_tid() {
-  // 1 to 3+N thread ID
-  return 0;
+    static int tid = 1;
+    return tid++;
 }
 
 void* producer_routine(void* arg) {
-  (void)arg;
-  // read data, loop through each value and update the value, notify consumer,
-  // wait for consumer to process
-  return nullptr;
+    int* data = static_cast<int*>(arg);
+
+    for (int i = 0; i < *data; i++) {
+        int value = std::rand() % 100;
+        pthread_mutex_lock(&MUTEX);
+        while (buffer.size() == BUFFER_SIZE) {
+            pthread_cond_wait(&CV_PRODUCER, &MUTEX);
+        }
+        buffer.push(value);
+        pthread_cond_signal(&CV_CONSUMER);
+        pthread_mutex_unlock(&MUTEX);
+    }
+
+    pthread_mutex_lock(&MUTEX);
+    while (buffer.size() != 0) {
+        pthread_cond_wait(&CV_PRODUCER, &MUTEX);
+    }
+    pthread_cond_broadcast(&CV_CONSUMER);
+    pthread_mutex_unlock(&MUTEX);
+
+    return nullptr;
 }
 
 void* consumer_routine(void* arg) {
-  (void)arg;
-  // for every update issued by producer, read the value and add to sum
-  // return pointer to result (for particular consumer)
-  return nullptr;
+    int* result = new int(0);
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, nullptr);
+
+    while (true) {
+        pthread_mutex_lock(&MUTEX);
+        while (buffer.empty()) {
+            pthread_cond_wait(&CV_CONSUMER, &MUTEX);
+            if (buffer.empty()) {
+                pthread_mutex_unlock(&MUTEX);
+                continue;
+            }
+        }
+        int value = buffer.front();
+        buffer.pop();
+        *result += value;
+        pthread_cond_signal(&CV_PRODUCER);
+        pthread_mutex_unlock(&MUTEX);
+    }
+
+    return result;
 }
 
 void* consumer_interruptor_routine(void* arg) {
-  (void)arg;
-  // interrupt random consumer while producer is running
-  return nullptr;
+    std::vector<pthread_t>* consumers = static_cast<std::vector<pthread_t>*>(arg);
+    while (true) {
+        usleep(20000);
+        if (buffer.empty()) {
+            continue;
+        }
+        int consumer_id = std::rand() % consumers->size();
+        pthread_cancel((*consumers)[consumer_id]);
+    }
+    return nullptr;
 }
 
-// the declaration of run threads can be changed as you like
-int run_threads() {
-  // start N threads and wait until they're done
-  // return aggregated sum of values
+int run_threads(int n_producers, int n_consumers) {
+    std::srand(std::time(nullptr));
 
-  return 0;
+    pthread_t producer_thread, interrupt_thread;
+    std::vector<pthread_t> consumer_threads(n_consumers);
+
+    int producer_data = n_producers * BUFFER_SIZE;
+    pthread_create(&producer_thread, nullptr, producer_routine, &producer_data);
+
+    for (int i = 0; i < n_consumers; i++) {
+        pthread_create(&consumer_threads[i], nullptr, consumer_routine, nullptr);
+    }
+
+    pthread_create(&interrupt_thread, nullptr, consumer_interruptor_routine, &consumer_threads);
+
+    pthread_join(producer_thread, nullptr);
+
+    void* result;
+    int sum = 0;
+    for (int i = 0; i < n_consumers; i++) {
+        pthread_join(consumer_threads[i], &result);
+        sum += *static_cast<int*>(result);
+        delete static_cast<int*>(result);
+    }
+
+    pthread_join(interrupt_thread, nullptr);
+
+    pthread_cond_destroy(&CV_CONSUMER);
+    pthread_cond_destroy(&CV_PRODUCER);
+    pthread_mutex_destroy(&MUTEX);
+
+    return sum;
 }
--- producer_consumer.h
+++ producer_consumer.h
@@ -1,4 +1,5 @@
 #pragma once
 
 // the declaration of run threads can be changed as you like
-int run_threads();
+int get_tid();
+int run_threads(int thread_count, int sleep_time, bool debug);
