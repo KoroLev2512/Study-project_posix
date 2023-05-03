22031d9d8eb2484773866b8e01e8c1fdf63a31dd
Korolev Yurii <yurii@niuitmo.ru>
2023-05-03 16:03:59 +0300

    version 3

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,4 +2,27 @@
 #include <doctest.h>
 #include <producer_consumer.h>
 
-TEST_CASE("just_example") { CHECK(4 == 4); }
+TEST_CASE("FinalTest") {
+    std::set<int> tids;
+    for (
+            int i = 1;
+            i <= 20; ++i) {
+        pthread_t thread;
+
+        auto lambda = [](void *arg) -> void * {
+            auto *tids = static_cast<std::set <int8_t> *>(arg);
+            tids->insert(get_tid());
+            return nullptr;
+        };
+
+    pthread_create(&thread, nullptr, lambda, &tids);
+    pthread_join(thread, nullptr);
+    }
+
+    std::set<int> tidsReference = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                               11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+    CHECK_EQ(*tids.begin(), *tidsReference.begin());
+    CHECK_EQ(*tids.rbegin(), *tidsReference.rbegin());
+    CHECK_EQ(tids.size(), tidsReference.size());
+}
